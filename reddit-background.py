e73ba79fcbb7e826581affc686720d1173a57f54
Christopher Nethercott <ccnethercott@gmail.com>
2022-12-23 10:54:05 +0000

    feat: remove directory of previous downloaded wallpapers

--- reddit-background.py
+++ reddit-background.py
@@ -1,55 +1,58 @@
 import argparse
 import logging
+import sys
 import yaml
 import requests
 import nanoid
-from voluptuous import Schema, Required, All, Length, Any, MultipleInvalid
+import os
+from voluptuous import Schema, Required, All, Length, Any, MultipleInvalid, IsDir
 
 config_schema = Schema({
     Required('subreddit'): All(str, Length(min=1, max=20)),
     Required('sort'): Any('top', 'hot'),
     'period': Any('hour', 'week', 'month', 'year', 'all'),
-    Required('amount'): All(int, Length(min=1, max=25))
+    Required('amount'): int,
+    Required('output'): IsDir()
 })
 
 
-def download_image(image):
+def clear_dir(dir):
+    file_list = [f for f in os.listdir(dir) if f.endswith(".jpg")]
+    for f in file_list:
+        os.remove(os.path.join(dir, f))
+
+
+def download_image(url):
     with open(f'backgrounds/{nanoid.generate()}.jpg', 'wb') as file:
-        file.write(requests.get(image['link']).content)
+        file.write(requests.get(url).content)
 
 
 def main():
     parser = argparse.ArgumentParser(description='Automatically download images from Reddit to use on your desktop.')
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
     parser.add_argument('--config', type=argparse.FileType('r'), required=True)
 
-    args = parser.parse_args()
+    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
 
+    args = parser.parse_args()
     config = yaml.safe_load(args.config)
 
     try:
         config_schema(config)
     except MultipleInvalid as e:
         logging.error(f'Configuration valid does not match schema: {str(e)}')
+        logging.error(e)
+        sys.exit(1)
 
-    print(config)
+    logging.info(f"Removing all jpg files in directory: {config.get('output')}")
+    clear_dir(config.get('output'))
 
     url = f"https://reddit.com/r/{config.get('subreddit')}/{config.get('sort')}.json"
     logging.info(f'Using URL {url}')
 
     subreddit = requests.get(url, headers={'User-Agent': 'github/chriscn/reddit-background'}, timeout=10).json()
-    print(subreddit['data']['children'])
-
-    image_urls = []
 
     for i in range(0, config['amount'] - 1):
-        image_urls.append({'link': subreddit['data']['children'][i]['data']['url'],
-                           'title': subreddit['data']['children'][i]['data']['title']})
-
-    print(image_urls)
-
-    for image in image_urls:
-        download_image(image)
+        download_image(subreddit['data']['children'][i]['data']['url'])
 
 
 if __name__ == '__main__':
