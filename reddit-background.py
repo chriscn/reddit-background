0a13ce768a88e5822c3f1e3c54e5a6e7d84e0e8b
Christopher Nethercott <ccnethercott@gmail.com>
2022-12-24 09:45:17 +0000

    fix: remove schema validation for now

--- reddit-background.py
+++ reddit-background.py
@@ -1,47 +1,27 @@
 import argparse
 import logging
-import sys
 import yaml
 import requests
 import nanoid
 import os
-from voluptuous import Schema, Required, All, Length, Any, MultipleInvalid, IsDir
-
-config_schema = Schema({
-    Required('subreddit'): All(str, Length(min=1, max=20)),
-    Required('sort'): Any('top', 'hot'),
-    'period': Any('hour', 'week', 'month', 'year', 'all'),
-    Required('amount'): int,
-    Required('output'): IsDir()
-})
-
-
-def clear_dir(dir):
-    file_list = [f for f in os.listdir(dir) if f.endswith(".jpg")]
+def clear_dir(directory):
+    file_list = [f for f in os.listdir(directory) if f.endswith(".jpg")]
     for f in file_list:
-        os.remove(os.path.join(dir, f))
-
-
+        logging.info(f'Removing file {f}')
+        os.remove(os.path.join(directory, f))
 def download_image(url):
     with open(f'backgrounds/{nanoid.generate()}.jpg', 'wb') as file:
+        logging.info(f'Downloading {url} to {file.name}')
         file.write(requests.get(url).content)
-
-
 def main():
     parser = argparse.ArgumentParser(description='Automatically download images from Reddit to use on your desktop.')
-    parser.add_argument('--config', type=argparse.FileType('r'), required=True)
+    parser.add_argument('--config', type=argparse.FileType('r'), required=True,
+                        help='Configuration file, have a look at the example provided.')
 
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
 
     args = parser.parse_args()
     config = yaml.safe_load(args.config)
-
-    try:
-        config_schema(config)
-    except MultipleInvalid as e:
-        logging.error(f'Configuration valid does not match schema: {str(e)}')
-        logging.error(e)
-        sys.exit(1)
 
     logging.info(f"Removing all jpg files in directory: {config.get('output')}")
     clear_dir(config.get('output'))
@@ -51,7 +31,7 @@ def main():
 
     subreddit = requests.get(url, headers={'User-Agent': 'github/chriscn/reddit-background'}, timeout=10).json()
 
-    for i in range(0, config['amount'] - 1):
+    for i in range(0, config.get('amount') - 1):
         download_image(subreddit['data']['children'][i]['data']['url'])
 
 
